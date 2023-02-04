d924511f2b73547616510f5f2a65941e917e15a0
Kavinda Weerasooriya <87388524+kavindaweerasooriya@users.noreply.github.com>
2023-02-04 10:22:25 +0530

    incorrect MIME type

--- Backend/api/models/provider.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const { providerSchema } = require('../schemas/provider.schemas');
-
-//create provider model
-
-const Provider = mongoose.model('provider', providerSchema);
-
-module.exports = { Provider }
